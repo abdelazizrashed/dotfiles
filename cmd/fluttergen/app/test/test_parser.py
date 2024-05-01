7eb1e7d25a62b02368c0a7d9ecebfe4e7780695a
Abdelaziz Rashed <aziz@192.168.1.3>
2024-05-01 21:17:49 +0300

    Clean fluttergen

--- cmd/fluttergen/app/test/test_parser.py
+++ cmd/fluttergen/app/test/test_parser.py
@@ -16,7 +16,6 @@ class TestParser(unittest.TestCase):
                 "phone": "123120",
                 "email": "fgsd01fg@yahoo.com",
                 "image": "https://tablefinder.top/Voucher_App/storage/image/80875091707126500lBvuPXFbe1apybjUdXeMQcdOWxZB4zwA55nkHBqU.png",
-                "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzIiwianRpIjoiNjkxZWFlY2E2MzBiN2U2MDY5NGQ2MzNjOThkNTk1NDEwMWY2ZjBkNTI4N2U4MWY5YWE5Zjk2MTRkZDI4MzA2ZjdkZGI1YjY5NDdmY2U5NmIiLCJpYXQiOjE3MDk2NTU5NDIuNjI3ODIzMTE0Mzk1MTQxNjAxNTYyNSwibmJmIjoxNzA5NjU1OTQyLjYyNzgyNzg4Mjc2NjcyMzYzMjgxMjUsImV4cCI6MTc0MTE5MTk0Mi42MjEyMjk4ODcwMDg2NjY5OTIxODc1LCJzdWIiOiI0OSIsInNjb3BlcyI6W119.m6RiNlZE56zWc8HpoHorQCeXDUt3e7TfUl2q-JdFwcirP8L1twpjDLuNYpVGhpMCExLTNh8fRKI7MDNoH54kYF3nxf9CKMf0FeJJ5Yw8addRH7VuOE9tkqYVUtCIM62HvO3o-vpb50x33aw4U2mtpG_ZoYvTiQ-Ka8LuR1Mpp46Mi4IEI9OKd7B7l3ZHes_EgHwAbjzIqzvaP2zQciVwRnhb_qYePQNhEX_0YIuV6mYFp7PKztHZvFFVfiOG6b2Cusxro-vqqLrAxbv5SA8VERiqrXCUDbVNjv-V3_YFkJ8Wr4xwIElG_jauZGsdy8cHmwGSFrOUPOXkk-CfQb4pxHm0-2J8rz0QqikrpxTq5-dSHmBU2Ak3AI1Aqo6WYfoQ2Q0r_ql9P8ImyM3IBc_-l4MOHDhrR85d8S0rI5Xe5b5gfjX7aZ6U34BHvCyZMS6mCDt1xSxCGzkIuChW7IiKd0BonFrh-p9x136aFi65Xp4-n5cAZRVgaoYBGdGssAGaMStqy1WYz-It9yaiGvOrA0TRAkk82q-_i5SSe1gZtQfTeJlVPWCaWaGHuRNkcdcW3794DDRpx4UzPIb5k95drIlxIG81_SpRIDjQpNOVblzAyj9WPu5kHxK8CnnSNwa_5mhRrC32Tr8DuMFzrR98EA3eYXRg1AmWlqVWZgKktXY",
                 "role": 2,
                 "role_name": "Customer"
             }
@@ -40,11 +39,3 @@ class TestParser(unittest.TestCase):
         self.assertEqual(parser.list_parser(value4, "user", "hi"), "List<UserHiModel>");
         
         
-    # def parse_type(self,value, parent: str, name: str) -> DataType :
-    #     pass
-    #
-    # def _list_parser(self,value, parent: str, name: str) -> str:
-    #     pass
-
-# if __name__ == '__main__':
-#     unittest.main()
